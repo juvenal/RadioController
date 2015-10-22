22578771d9812a21361ec959d16e3eaacba998e3
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2015-10-22 16:05:13 -0200

    Add APData Info collector
    This commit adds a logical representation for each managed wifi access
    point.

---
+++ APData/APInfo.py
@@ -0,0 +1,44 @@
+#
+#
+#
+#
+
+class APInfo:
+	"""..."""
+	
+	# Protected members
+	__IPAddress = ""
+	__MACAddress = ""
+	__Channel = 0
+	__Region = 0
+	__Localization = ""
+	__TxPowerList = []
+	__CurrentPowerIndex = -1
+	__UnderloadLimit = -1
+	__OverloadLimit = -1
+	__Reachable = False
+	__Enabled = False
+	__EMailSent = False
+	__SupportedOS = ""
+
+	# Public members mirrors
+
+	# Class initialization
+	def __init__(self):
+		#
+
+	# Set the AP transmission power (Tx)
+	def updateTxPowerIndex(self, newTxPower):
+		#
+		if newTxPower < 0:
+			self.CurrentPowerIndex = len(self.TxPowerList) - 1
+		else:
+			for powerTxIndex in self.TxPowerList:
+				if newTxPower > self.TxPowerList[powerTxIndex]:
+					break
+			self.CurrentPowerIndex = powerTxIndex
+
+
+# Heroku: User: juvenal-jr@ig.com.br / Senha: w4cpvX3DWw
+
+# wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
